265fa73d976ae943a6b29ef612fb755b95087789
Mychal <Mychal_Clark@insideranken.org>
2021-11-18 14:27:41 -0600

    pet editor

--- src/components/PetEditor.js
+++ src/components/PetEditor.js
@@ -1,11 +1,36 @@
-import {useParams} from 'react-router-dom'
-function PetEditor(){
 
-    const {petId} = useParams();
-    return(
-        <div> <h1>Pet Editor</h1>
-        {petId}
-        </div>
-    )
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import _ from 'lodash';
+
+function PetEditor({ auth, showError }) {
+  const { petId } = useParams();
+  const [pet,setPet] = useState(null);
+  const [error,setError]=useState('');
+
+  useEffect(() => {
+    axios(`${process.env.REACT_APP_API_URL}/api/pet/${petId}`, {
+      method: 'get',
+      headers: {
+        authorization: `Bearer ${auth?.token}`,
+      },
+    })
+      .then((res) => {
+        console.log(res.data);
+        setPet(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+        showError(err.message)
+      });
+  }, [auth, petId]);
+  return (
+    <div>
+      {' '}
+      <h1>Pet Editor</h1>
+      {petId}
+    </div>
+  );
 }
 export default PetEditor;
